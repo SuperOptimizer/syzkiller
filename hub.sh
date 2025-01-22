a19646c009c79b66a065d6df60145e06adcf8d5b
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-01-22 17:27:18 -0500

    fixes

--- hub.sh
+++ hub.sh
@@ -4,5 +4,4 @@ set -eux
 cd /syzkiller
 cd hubworkdir
 wget https://github.com/cmu-pasta/linux-kernel-enriched-corpus/releases/download/latest/corpus.db
-nohup syzhub ./syzkaller/bin/syz-hub -config hub.cfg &> out_hub.txt &
-echo "Hub started"
+./syzkaller/bin/syz-hub -config hub.cfg &> out_hub.txt
