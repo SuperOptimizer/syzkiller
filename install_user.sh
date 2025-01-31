f05e844d0126f542efca51fcf2464a3899451647
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-01-31 15:24:47 -0500

    completely delete and recheckout syzkaller and linux

--- install_user.sh
+++ install_user.sh
@@ -4,5 +4,8 @@
 export PATH=/usr/local/go/bin:$PATH
 cd /syzkiller
 
+rm -rf linux
+rm -rf syzkaller
+
 git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 git clone --depth 1 https://github.com/google/syzkaller
