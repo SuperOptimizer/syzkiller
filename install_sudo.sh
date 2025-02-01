eac9cd40f3ce1116fcf2c13d54fe769b28ef2ce1
SuperOpt <156155735+SuperOptimizer@users.noreply.github.com>
2025-01-31 19:50:38 -0600

    Update install_sudo.sh

--- install_sudo.sh
+++ install_sudo.sh
@@ -10,6 +10,10 @@ ln -s /usr/bin/llvm-nm-20 /usr/local/bin/llvm-nm
 ln -s /usr/bin/llvm-ar-20 /usr/local/bin/llvm-ar
 ln -s /usr/bin/clang-20 /usr/local/bin/clang
 
+sudo apt update -y
+sudo apt install -y clang-20 lldb-20 lld-20 clangd-20 clang-tidy-20 clang-format-20 clang-tools-20 llvm-20-dev lld-20 lldb-20 llvm-20-tools libomp-20-dev libc++-20-dev libc++abi-20-dev libclang-common-20-dev libclang-20-dev libclang-cpp20-dev libunwind-20-dev libclang-rt-20-dev libpolly-20-dev
+
+
 GO_VERSION="1.21.5"
 wget "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
 rm -rf /usr/local/go
