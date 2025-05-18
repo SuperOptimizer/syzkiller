ff6343ba093e73183d63e1af5cb39ebc41e99912
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-05-18 08:24:40 -0500

    add the current user to the kvm group

--- install_sudo.sh
+++ install_sudo.sh
@@ -1,13 +1,13 @@
 #assumes ubuntu 25.04
 
 set -x
-apt-get update
+apt-get update -y
 apt-get install -y git curl make gcc wget g++ flex bison libelf-dev libssl-dev pkg-config debootstrap software-properties-common qemu-system
 
 
-sudo apt update -y
 sudo apt install -y clang-20 lldb-20 lld-20 clangd-20 clang-tidy-20 clang-format-20 clang-tools-20 llvm-20-dev lld-20 lldb-20 llvm-20-tools libomp-20-dev libc++-20-dev libc++abi-20-dev libclang-common-20-dev libclang-20-dev libclang-cpp20-dev libunwind-20-dev libclang-rt-20-dev libpolly-20-dev
 
+sudo usermod -a -G kvm $USER
 
 GO_VERSION="1.24.3"
 wget "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
