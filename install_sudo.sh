2c443af4ea7319dccaaa19ca6e768f9c13c91709
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-05-18 08:27:09 -0500

    install more stuff

--- install_sudo.sh
+++ install_sudo.sh
@@ -3,7 +3,7 @@
 set -x
 apt-get update -y
 apt-get install -y git curl make gcc wget g++ flex bison libelf-dev libssl-dev pkg-config debootstrap software-properties-common qemu-system
-
+sudo apt install -y build-essential gcc g++ llvm clang lld cmake ninja-build git gdb autoconf automake libtool pkg-config libncurses-dev libssl-dev make python3-dev
 
 sudo apt install -y clang-20 lldb-20 lld-20 clangd-20 clang-tidy-20 clang-format-20 clang-tools-20 llvm-20-dev lld-20 lldb-20 llvm-20-tools libomp-20-dev libc++-20-dev libc++abi-20-dev libclang-common-20-dev libclang-20-dev libclang-cpp20-dev libunwind-20-dev libclang-rt-20-dev libpolly-20-dev
 
