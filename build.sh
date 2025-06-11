d8ac1d21ef1801c56389145191bee29d893695ff
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-06-10 21:20:28 -0500

    simpler build sh for just asan / ubsan

--- build.sh
+++ build.sh
@@ -1,54 +1,18 @@
 #!/bin/bash
 set -x
 export PATH=/usr/local/go/bin:$PATH
-SANITIZER="kasan"
-KERNEL_DIR="linux"
 
-usage() {
-    echo "Usage: $0 [--sanitizer TYPE]"
-    echo "Sanitizer types: kasan, kcsan, kmsan, lsan"
-    exit 1
-}
+cd /syzkiller
+rm -rf linux
+rm -rf syzkaller
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --sanitizer)
-            SANITIZER="$2"
-            shift 2
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
+git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+git clone --depth 1 https://github.com/google/syzkaller
 
-setup_kernel() {
-    cd /syzkiller
-    rm -rf linux
-    rm -rf syzkaller
+CONFIG_FILE="/syzkiller/syzkaller/dashboard/config/linux/upstream-kasan-badwrites.config"
 
-    git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-    git clone --depth 1 https://github.com/google/syzkaller
-    
-    # Determine which config file to use and extra config lines based on SANITIZER
-    CONFIG_FILE=""
-    EXTRA_CONFIG=""
-    case "$SANITIZER" in
-        kasan)
-            CONFIG_FILE="/syzkiller/syzkaller/dashboard/config/linux/upstream-smack-kasan.config"
-            EXTRA_CONFIG=""
-            ;;
-        kcsan)
-            CONFIG_FILE="/syzkiller/syzkaller/dashboard/config/linux/upstream-kcsan.config"
-            EXTRA_CONFIG=""
-            ;;
-        kmsan)
-            CONFIG_FILE="/syzkiller/syzkaller/dashboard/config/linux/upstream-kmsan.config"
-            EXTRA_CONFIG=""
-            ;;
-        ubsan)
-            CONFIG_FILE="/syzkiller/syzkaller/dashboard/config/linux/upstream-kasan-badwrites.config"
-            EXTRA_CONFIG="
+EXTRA_CONFIG="
+
 CONFIG_UBSAN=y
 CONFIG_UBSAN_ALIGNMENT=y
 CONFIG_UBSAN_BOOL=y
@@ -61,31 +25,16 @@ CONFIG_UBSAN_SHIFT=y
 CONFIG_UBSAN_SIGNED_WRAP=y
 CONFIG_UBSAN_TRAP=y
 CONFIG_UBSAN_UNREACHABLE=y
-CONFIG_KASAN=n
+
 "
-            ;;
-        *)
-            echo "Error: Unknown sanitizer type: $SANITIZER"
-            exit 1
-            ;;
-    esac
-    cd /syzkiller/linux
-    cp "$CONFIG_FILE" .config
-    if [ -n "$EXTRA_CONFIG" ]; then
-        echo "Appending additional configuration..."
-        echo "$EXTRA_CONFIG" >> .config
-    fi
-    make olddefconfig LLVM=1
-    make -j$(nproc) LLVM=1
-}
 
-build_syzkaller() {
-    cd /syzkiller/syzkaller
-    git pull
-    make
-}
 
-setup_kernel
-build_syzkaller
-#cd /syzkiller/workdir
-#echo "Manager started"
+cd /syzkiller/linux
+cp "$CONFIG_FILE" .config
+echo "$EXTRA_CONFIG" >> .config
+make olddefconfig
+make -j$(nproc)
+
+cd /syzkiller/syzkaller
+git pull
+make
